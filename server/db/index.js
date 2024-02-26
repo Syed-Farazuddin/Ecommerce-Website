bac81c192f4dbc66dfad44d2c9218014d2af2b55
Syed Faraz <syedfarazuddin73@gmail.com>
2024-02-26 20:30:02 +0530

    stuck at hashing haha

---
+++ server/db/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ecommerce")
+  .then(() => {
+    console.log("Connected to database".bgWhite.blue);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+require("./db/index");
+const morgan = require("morgan");
+const authRoutes = require("./routes/authRoute");
+
+dotenv.config();
 const app = express();
+
+// Middlewares
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello server");
 });
 
-app.listen(4000, () => {
-  console.log("Server started on port 4000");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`.bgMagenta.white);
 });
